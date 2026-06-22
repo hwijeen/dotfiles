5d933f29bc2fc9889bb36504eaeab8bf08d62fb8
hwijeen <hwijeen@gmail.com>
2026-06-22 09:25:22 -0700

    fix symlink create

--- create_link.sh
+++ create_link.sh
@@ -1,5 +1,9 @@
-ln -sf ~/dotfiles/.vimrc ~/.
-ln -sf ~/dotfiles/.zshrc ~/.
-ln -sf ~/dotfiles/.gitconfig ~/.
-ln -sf ~/dotfiles/.tmux.conf ~/.
-ln -sf ~/dotfiles/config ~/.ssh/config
+#!/usr/bin/env bash
+set -euxo pipefail
+
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+ln -sf "$DOTFILES_DIR/.vimrc" "$HOME/.vimrc"
+ln -sf "$DOTFILES_DIR/.zshrc" "$HOME/.zshrc"
+ln -sf "$DOTFILES_DIR/.gitconfig" "$HOME/.gitconfig"
+ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
